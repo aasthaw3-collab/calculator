6e633b83a1c333d3899499a4f7ec25133b8f63cd
aasthaw3-collab <aasthaw3@gmail.com>
2026-09-19 17:28:27 +0530

    Add files via upload
    added calculator program

---
+++ calculator.py
@@ -0,0 +1,12 @@
+n=int(input("enter num1"))
+m=int(input("enter num 2"))
+sum=n+m
+division=n/m
+product=n*m
+subtraction=n-m
+square=n**2
+print("sum of numbers is :",sum)
+print("division of numbers is :",division)
+print("product of numbers is :",product)
+print("subtraction of numbers is :",subtraction)
+print("square of num1 is :",square)
